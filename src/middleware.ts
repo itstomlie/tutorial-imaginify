8c3d8b2a509412cf20c6621820314c48ea614714
Tommy <itsTomLie@gmail.com>
2024-07-10 14:48:40 +0800

    implement clerk webhooks

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,3 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 export default clerkMiddleware((auth, req) => {});
-
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
