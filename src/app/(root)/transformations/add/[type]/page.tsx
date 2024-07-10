73f3b38d927586d9841401cf19859837ba3d491c
Tommy <itsTomLie@gmail.com>
2024-07-10 14:58:43 +0800

    implement clerk webhooks

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,13 +1,14 @@
 // import Header from "@/components/shared/Header";
-// import React from "react";
+import React from "react";
 
-// const AddTransformationTypePage = () => {
-//   return (
-//     <Header
-//       title="Add Transformation Type"
-//       subtitle="Add a new transformation type"
-//     />
-//   );
-// };
+const AddTransformationTypePage = () => {
+  return (
+    // <Header
+    //   title="Add Transformation Type"
+    //   subtitle="Add a new transformation type"
+    // />
+    <div>Add Transformation Type</div>
+  );
+};
 
-// export default AddTransformationTypePage;
+export default AddTransformationTypePage;
